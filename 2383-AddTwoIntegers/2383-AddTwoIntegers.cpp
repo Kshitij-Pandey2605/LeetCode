0f7389743c8628063cf1848ffabf8d1b598fa50b
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-29 16:08:04 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/29/2026, 4:08:06 PM
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        int total =0;
+        total = num1+num2;
+        return total;
+    }
+};
