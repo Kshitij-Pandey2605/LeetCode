3523700b47d63926181757a7235be279d86a8286
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-30 17:12:16 +0530

    Create 1630-CountOddNumbersInAnIntervalRange

---
+++ 1630-CountOddNumbersInAnIntervalRange/1630-CountOddNumbersInAnIntervalRange.cpp
@@ -0,0 +1,13 @@
+// Last updated: 7/30/2026, 5:12:16 PM
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int count =0;
+        for(int i=low;i<=high;++i){
+            if(i%2!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
