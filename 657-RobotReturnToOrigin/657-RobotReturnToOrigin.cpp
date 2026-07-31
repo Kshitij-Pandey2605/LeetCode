f519a2c90fa147b99d5e822fa56b37949025f7a8
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:19:36 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/31/2026, 9:19:36 PM
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        
+        for(int i=0;i<moves.length();++i){
+            if(moves[i]=='U'){
+                y++;
+            }
+             if(moves[i]=='D'){
+                y--;
+            }
+             if(moves[i]=='L'){
+                x--;
+            }
+             if(moves[i]=='R'){
+                x++;
+            }
+        }
+        return (x==0&&y==0);
+
+    }
+};
