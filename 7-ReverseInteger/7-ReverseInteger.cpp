f9180216d8137a1ebbb50e5ac48cc5dd51826d65
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:25:35 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2026, 9:25:35 PM
+class Solution {
+public:
+  int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        x = x / 10;
+
+        // overflow check
+        if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
+            return 0;
+
+        rev = rev * 10 + digit;
+    }
+
+    return rev;
+}
+};
