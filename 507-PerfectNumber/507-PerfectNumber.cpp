2ec87a44fd08fb0f398381fee16314775e099f85
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:19:54 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/31/2026, 9:19:54 PM
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+
+        int sum=0;
+        for(int i=1;i<num;++i){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        
+        return sum==num;
+    }
+};
