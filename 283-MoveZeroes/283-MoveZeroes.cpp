ca528e37d739d6d72b6271b54c0fbd58080adf1d
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:20:38 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/31/2026, 9:20:37 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for (int i = 0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+    
+    while (j<nums.size()){
+        nums[j++]=0;
+    }
+    }
+};
