2adac2bbe9241222712ca02323e4c0584fb93d9e
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:23:36 +0530

    Create 168-ExcelSheetColumnTitle

---
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/31/2026, 9:23:36 PM
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        string result = "";
+
+        while(columnNumber > 0){
+
+            columnNumber--;
+
+            int rem = columnNumber % 26;
+
+            result += char('A' + rem);
+
+            columnNumber = columnNumber / 26;
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
