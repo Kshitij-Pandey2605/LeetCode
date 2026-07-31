92e4d7e89700c5d290218794713f9306eb85acf0
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:25:17 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 9:25:17 PM
+class Solution {
+public:
+    int romanToInt(string s) {
+    unordered_map<char,int>mp={
+        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+    };
+    int result=0;
+    for(int i=0;i<s.length();++i){
+        if(i<s.length()-1&&mp[s[i]]<mp[s[i+1]]){
+            result=result-mp[s[i]];
+        }
+        else{
+            result=result+mp[s[i]];
+        }
+    }
+    return result;
+    }
+};
