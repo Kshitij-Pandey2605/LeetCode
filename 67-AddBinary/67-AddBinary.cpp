3e18cf6c70e894ad52631bdfebe2eb682d26cb39
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:24:27 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/31/2026, 9:24:27 PM
+class Solution {
+public:
+    string addBinary(string a, string b) {
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        int carry = 0;
+
+        string ans = "";
+
+        while(i >= 0 || j >= 0 || carry) {
+
+            int sum = carry;
+
+            if(i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+
+            if(j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            ans += (sum % 2) + '0';
+
+            carry = sum / 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
