b943cdb757ec1c17678701f78ffccd9d1592a9a2
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-07-31 21:20:45 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2026, 9:20:45 PM
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0){
+            return false;
+            
+        }
+       while(n%2==0){
+        n=n/2;
+       }
+        while(n%3==0){
+        n=n/3;
+       }
+        while(n%5==0){
+        n=n/5;
+       }
+       return n==1;
+    }
+};
