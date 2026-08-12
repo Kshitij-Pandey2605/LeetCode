8bb9dd2fd8ba821ec06e99dad72c546a5170420d
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-08-12 09:50:49 +0530

    File updated at 8/12/2026, 9:50:48 AM

--- 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.cpp
+++ 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/11/2026, 2:14:51 PM
+// Last updated: 8/12/2026, 9:50:48 AM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -12,19 +12,39 @@
 11class Solution {
 12public:
 13    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-14        unordered_set<int> numSet(nums.begin(), nums.end());
-15        
-16        ListNode dummy(0, head);
-17        ListNode* curr = &dummy;
-18        
-19        while (curr->next != nullptr) {
-20            if (numSet.count(curr->next->val)) {
-21                curr->next = curr->next->next;
-22            } else {
-23                curr = curr->next;
-24            }
-25        }
-26        
-27        return dummy.next;
-28    }
-29};
+14
+15        ListNode* curr = head;
+16        ListNode* prev = nullptr;
+17        unordered_set<int>st(nums.begin(), nums.end());
+18
+19        while (curr != nullptr) {
+20
+21
+22            // Check whether curr->val exists in nums
+23            if(st.count(curr->val)){
+24
+25            
+26
+27                // If deleting head
+28                if (prev == nullptr) {
+29                    head = curr->next;
+30                    
+31                    curr = head;
+32                }
+33
+34                // If deleting middle/end node
+35                else {
+36                    prev->next = curr->next;
+37                    curr = prev->next;
+38                }
+39
+40            } else {
+41                prev = curr;
+42                curr = curr->next;
+43            }
+44        }
+45
+46        return head;
+47    
+48    }
+49};
