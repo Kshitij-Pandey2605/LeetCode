d827d266d93f3283c8c5b903bbbd73e1c3f6e9fd
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-08-13 16:16:20 +0530

    File updated at 8/13/2026, 4:16:20 PM

--- 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.cpp
+++ 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.cpp
@@ -1,37 +1,21 @@
-// Last updated: 8/13/2026, 1:37:43 PM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    vector<int> nextLargerNodes(ListNode* head) {
-14        vector<int>result;
-15
-16        ListNode*temp=head->next;
-17
-18        while(head!=nullptr){
-19            ListNode*temp=head->next;
-20            bool found = false;
-21         
-22         while(temp!=nullptr){
-23            if(temp->val>head->val){
-24                result.push_back(temp->val);
-25               found = true;
-26               break;
-27            }
-28            temp=temp->next;
-29        }
-30           if(!found){result.push_back(0);}
-31
-32           head=head->next;
-33        }
-34        return result;
-35    }
-36};
+// Last updated: 8/13/2026, 4:16:20 PM
+1class Solution {
+2public:
+3    int subarraysDivByK(vector<int>& nums, int k) {
+4        vector<int> remainder_count(k, 0);
+5        
+6        remainder_count[0] = 1;
+7        
+8        int prefix_sum = 0;
+9        int result = 0;
+10        
+11        for (int num : nums) {
+12            prefix_sum += num;
+13            int remainder = ((prefix_sum % k) + k) % k;
+14            result += remainder_count[remainder];           
+15            remainder_count[remainder]++;
+16        }
+17        
+18        return result;
+19    }
+20};
