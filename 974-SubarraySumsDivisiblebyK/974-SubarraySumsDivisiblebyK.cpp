65a187f15677ed84f415a9f77eb45c433304c282
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-08-15 23:08:22 +0530

    Create 974-SubarraySumsDivisiblebyK

---
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/15/2026, 11:08:23 PM
+1class Solution {
+2public:
+3    int subarraysDivByK(vector<int>& nums, int k) {
+4        vector<int> remainder_count(k, 0);
+5        
+6        remainder_count[0] = 1;
+7        
+8        int prefix_sum = 0;
+9        int result = 0;
+10        
+11        for (int num : nums) {
+12            prefix_sum += num;
+13            int remainder = ((prefix_sum % k) + k) % k;
+14            result += remainder_count[remainder];           
+15            remainder_count[remainder]++;
+16        }
+17        
+18        return result;
+19    }
+20};
