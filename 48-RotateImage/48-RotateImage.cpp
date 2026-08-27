1970613bd8cc0296a25d1a893bd565ab71c44f30
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-08-27 22:56:22 +0530

    File updated at 8/27/2026, 10:56:21 PM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,19 +1,58 @@
-// Last updated: 8/25/2026, 3:39:35 PM
-1class Solution {
-2public:
-3    void rotate(vector<vector<int>>& matrix) {
-4        int n = matrix.size();
-5
-6        // Step 1: Transpose the matrix
-7        for (int i = 0; i < n; i++) {
-8            for (int j = i + 1; j < n; j++) {
-9                swap(matrix[i][j], matrix[j][i]);
-10            }
-11        }
-12
-13        // Step 2: Reverse every row
-14        for (int i = 0; i < n; i++) {
-15            reverse(matrix[i].begin(), matrix[i].end());
-16        }
-17    }
-18};
+// Last updated: 8/27/2026, 10:56:21 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+14        stack<int>st1;
+15        stack<int>st2;
+16
+17           while (l1 != NULL) {
+18            st1.push(l1->val);
+19            l1 = l1->next;
+20        }
+21
+22
+23           while (l2 != NULL) {
+24            st2.push(l2->val);
+25            l2 = l2->next;
+26        }
+27
+28          int carry = 0;
+29        ListNode* head = NULL;
+30
+31
+32         while (!st1.empty() || !st2.empty() || carry != 0) {
+33
+34            int sum = carry;
+35
+36            if (!st1.empty()) {
+37                sum += st1.top();
+38                st1.pop();
+39            }
+40
+41            if (!st2.empty()) {
+42                sum += st2.top();
+43                st2.pop();
+44            }
+45
+46            int digit = sum % 10;
+47            carry = sum / 10;
+48
+49            // Insert new node at front
+50            ListNode* newNode = new ListNode(digit);
+51            newNode->next = head;
+52            head = newNode;
+53        }
+54
+55        return head;
+56    }
+57};
