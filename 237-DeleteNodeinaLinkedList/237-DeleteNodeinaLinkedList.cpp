83c6933a2be637f9cbb663294ec223325a710d6f
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-09-07 14:09:14 +0530

    File updated at 9/7/2026, 2:09:13 PM

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.cpp
@@ -1,27 +1,35 @@
-// Last updated: 8/4/2026, 2:01:44 PM
+// Last updated: 9/7/2026, 2:09:13 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
 4 *     int val;
 5 *     ListNode *next;
-6 *     ListNode(int x) : val(x), next(NULL) {}
-7 * };
-8 */
-9
-10
-11 
-12class Solution {
-13public:
-14    void deleteNode(ListNode* node) {
-15     ListNode*curr=node;
-16
-17     curr->val=curr->next->val;
-18      ListNode*temp=curr->next;
-19     curr->next=curr->next->next;
-20      delete temp;
-21     return;
-22
-23
-24
-25    }
-26};
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* removeElements(ListNode* head, int val) {
+14        ListNode* dummy = new ListNode(0);
+15        dummy->next = head;
+16        ListNode* pre = dummy;
+17         ListNode* slow = head;
+18      
+19
+20        while(slow!=NULL){
+21            
+22            if(slow->val==val){
+23                pre->next=slow->next;
+24                slow=pre->next;
+25            }
+26            else{
+27                pre=slow;
+28                slow=slow->next;
+29            }
+30            
+31        }
+32        return dummy->next;
+33    }
+34};
