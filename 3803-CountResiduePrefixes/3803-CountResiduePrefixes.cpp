abf46d6e3b76681f050ada2186732b5a319af661
Kshitij Pandey <kshitij.pandey.cg@gmail.com>
2026-09-10 15:16:01 +0530

    Create 3803-CountResiduePrefixes

---
+++ 3803-CountResiduePrefixes/3803-CountResiduePrefixes.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/10/2026, 3:16:01 PM
+1class Solution {
+2public:
+3    int residuePrefixes(string s) {
+4        unordered_set<int>hh;
+5        int ans=0;
+6
+7        for(int i =0;i<s.length();++i){
+8          hh.insert(s[i]);
+9          if(hh.size()==(i+1)%3){
+10            ans++;
+11          }
+12
+13        }
+14        return ans;;
+15    }
+16};
